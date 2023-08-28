fe38fb8b8e8eaea19185479593c225050338b26b
nmeri17 <vainglories17@gmail.com>
2023-08-28 17:24:36 +0100

    Clean up readme

--- README.md
+++ README.md
@@ -6,54 +6,6 @@
 
 1. php artisan migrate --seed
 
-## helpful commands
-php artisan make:controller TasksController --model=Tasks --resource --requests
+1. npm run build
 
-php artisan make:controller CategoryController --model=Category --resource --requests
-
-php artisan make:migration create_tasks_table
-
-php artisan make:migration create_task_category_table
-
-php artisan serve
-Laravel development server started: <http://127.0.0.1:8000>
-
-You can use any GraphQL client with Lighthouse, make sure to point it to the URL defined in the config. By default, the endpoint lives at http://localhost:8000/graphql
-
-php artisan inertia:start-ssr
-
-Next, you may navigate to your application's /login or /register URLs in your web browser. All of Breeze's routes are defined within the routes/auth.php file.
-
-## challenges
-
-1) writing the sdl (partially done. when complete, do 2 through phpunit. when that's working, do 3)
-2) crafting the front end queries
-	- {
-	  users {
-	    data {id
-	    email
-	    name}
-	  }
-	}
-
-	{
-	  user(id: 1) {
-	    email
-	    name
-	  }
-	}
-
-	- mutation {
-	  createUser(
-	    name:"John Doe"
-	    email:"john.doe@example.com"
-	    password: "secret"
-	  ) {
-	    id
-	    name
-	    email
-	  }
-	}
-3) connecting it through inertia
-4) adding the vue components
-5) how to test
+1. php artisan serve, then visit http://127.0.0.1:8000
--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -2,6 +2,7 @@
 import BreezeAuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Head, Link, useForm } from "@inertiajs/vue3";
+import gql from "graphql";
 
 const form = useForm({});
 
@@ -12,6 +13,8 @@ function destroy(id) {
 }
 
 const categories = [];
+
+// the apolloQuery doesn't work cuz apollo provider is not properly mounted on app instance. I don't know how to connect it cuz the api on the doc is different from createInertiaApp
 </script>
 
 <template>
@@ -79,70 +82,57 @@ const categories = [];
 																</thead>
 																<tbody>
 
-																	<ApolloQuery :query="require('../../graphql/queries/Tasks.gql')">
-														    <template v-slot="{ result: { loading, error, data } }">
-														      <!-- Loading -->
-														      <div v-if="loading" class="loading apollo">Loading...</div>
+																	<tr
+																			v-for="task in tasks"
+																			:key="task.id"
+																			class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+																	>
+																			<th
+																					scope="row"
+																					class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+																			>
+																					{{ task.id }}
+																			</th>
+																			<th
+																					scope="row"
+																					class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+																			>
+																					{{ task.title }}
+																			</th>
+																			<td class="px-6 py-4">
+																					{{ task.status }}
+																			</td>
+																			<td class="px-6 py-4">
+																					{{ task.description }}
+																			</td>
+																			<td class="px-6 py-4">
+																					{{ task.due_date }}
+																			</td>
+																			<td class="px-6 py-4">
+																					{{ task.category.name }}
+																			</td>
 
-														      <!-- Error -->
-														      <div v-else-if="error" class="error apollo">An error occurred</div>
 
-														      <!-- Result -->
-														      <div v-else-if="data" class="result apollo">
-																					<tr
-																							v-for="task in data"
-																							:key="task.id"
-																							class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+																			<td class="px-6 py-4">
+																					<Link
+																							:href="
+																									route(
+																											'tasks.edit',
+																											task.id
+																									)
+																							"
+																						 class="px-4 py-2 text-white bg-blue-600 rounded-lg" >Edit</Link
 																					>
-																							<th
-																									scope="row"
-																									class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-																							>
-																									{{ task.id }}
-																							</th>
-																							<th
-																									scope="row"
-																									class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-																							>
-																									{{ task.title }}
-																							</th>
-																							<td class="px-6 py-4">
-																									{{ task.status }}
-																							</td>
-																							<td class="px-6 py-4">
-																									{{ task.description }}
-																							</td>
-																							<td class="px-6 py-4">
-																									{{ task.due_date }}
-																							</td>
-																							<td class="px-6 py-4">
-																									{{ task.category.name }}
-																							</td>
-
-
-																							<td class="px-6 py-4">
-																									<Link
-																											:href="
-																													route(
-																															'tasks.edit',
-																															task.id
-																													)
-																											"
-																										 class="px-4 py-2 text-white bg-blue-600 rounded-lg" >Edit</Link
-																									>
-																							</td>
-																							<td class="px-6 py-4">
-																									<PrimaryButton
-																											class="bg-red-700"
-																											@click="destroy(task.id)"
-																									>
-																											Delete
-																									</PrimaryButton>
-																							</td>
-																					</tr>
-																				</div>
-																			</template>
-																	</ApolloQuery>
+																			</td>
+																			<td class="px-6 py-4">
+																					<PrimaryButton
+																							class="bg-red-700"
+																							@click="destroy(task.id)"
+																					>
+																							Delete
+																					</PrimaryButton>
+																			</td>
+																	</tr>
 																</tbody>
 														</table>
 												</div>
